3efbf0f7b0141bfa4b615934ef2dfbd8e97d1a79
Annie-Sui <aqs2104@columbia.edu>
2019-01-17 11:00:39 -0500

    added website files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('p').on("mouseover", function() {
+    	$(this).css({
+    		color: "#42cbad",
+    		"font-weight": "700",
+    		"line-height": "40px"
+    	});
+    });
+});
+
