63b9d49cb4014f2423dc44bf7e43fb786d5c6828
Annie Sui <breadou@dyn-160-39-209-13.dyn.columbia.edu>
2019-07-08 16:49:47 -0400

    upload take 2 for css fix

---
+++ js/helpers.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	// $("#trigger").click(function(){
+	// $("#demo").html("Hello world");
+	// })
+	console.log("document ready");
+	loadHome();
+});
+
+var loadHome = function(){
+	
+}
