9ababdad4b83f7210987a0c2d21213d164f72ae9
Annie Sui <breadou@Annies-MacBook.local>
2019-07-16 16:34:02 -0400

    site now dynamically loads using javascript

--- js/data.js
+++ js/data.js
@@ -1,6 +1,39 @@
+
 var data = {
+	"id1": {
+		num: "Image 1: ",
+		id: "Coke Ferris Wheel",
+		image_src: "img/coke-ferris-wheel-final.gif"
+	},
 
+	"id2":{
+		num: "Image 2: ",
+		id: "Condom Action",
+		image_src: "img/condom-action-final.gif"
+	},
 
+	"id3":{
+		num: "Image 3: ",
+		id: "Dove Love",
+		image_src: "img/dove-love-final.gif"
+	},
 
+	"id4":{
+		num: "Image 4: ",
+		id: "Feminism",
+		image_src: "img/feminism-final.gif"
+	},
+
+	"id5":{
+		num: "Image 5: ",
+		id: "Food Puzzle",
+		image_src: "img/food-puzzle-final.gif"
+	},
+
+	"id6":{
+		num: "Image 6: ",
+		id: "Fries Lighting",
+		image_src: "img/fries-lighting-final.gif"
+	}
 
 }
--- js/helpers.js
+++ js/helpers.js
@@ -1,12 +1,78 @@
+var count = 0;
 
 $(document).ready(function(){
-	$("#trigger").click(function(){
-	$("#demo").append(" nothing much");
-	})
-	console.log("Document is ready");
-	// loadHome();
+	console.log("Document is ready!");
+	loadHome();
 });
 
-// var loadHome = function(){
-	
-// }
+function loadDataLinear(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	console.log(Object.keys(data).length);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+		var caption = item.num + item.id;
+		container_div.append("<br>" + caption + "<br>");
+		var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+		container_div.append(image);
+
+		if (currNum <= total){
+			currNum++;
+			col = $("<div>");
+			col.attr("class", "col-md-4");
+
+			$(row).append(col);
+			$(col).html(container_div);
+
+		}
+
+
+	}
+
+	changeCount();
+
+
+
+}
+
+var setActiveLinear = function(type){
+	console.log("setActiveLinear");
+	changeHeader('Linear');
+	loadDataLinear();
+
+}
+
+var changeCount = function(){
+	$("#counter").empty();
+	$("#counter").append('Number of Animations: ' + count);
+}
+
+var changeHeader = function(name){
+	$("#header").empty();
+	$("#header").append(name);
+}
+
+var loadHome = function(){
+	$("#header").empty();
+	$("#counter").empty();
+	$("#parent").html("");
+	$("#homepage").show();
+}
