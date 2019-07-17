cbbd67427ff0525bff3315f270970eac2db4abd2
Annie Sui <breadou@dyn-160-39-208-156.dyn.columbia.edu>
2019-07-17 13:23:03 -0400

    added speed categorization

--- js/data.js
+++ js/data.js
@@ -4,218 +4,256 @@ var data = {
 		num: "Image 1: ",
 		id: "Coke Ferris Wheel",
 		image_src: "img/coke-ferris-wheel-final.gif",
-		category: ["spin"]
+		movement: ["spin"],
+		looping: ["infinite"],
+		speed: ["uniform"]
 	},
 
 	"id2":{
 		num: "Image 2: ",
 		id: "Condom Action",
 		image_src: "img/condom-action-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["return"]
 	},
 
 	"id3":{
 		num: "Image 3: ",
 		id: "Dove Love",
 		image_src: "img/dove-love-final.gif",
-		category: ["spin"]
+		movement: ["spin"],
+		looping: ["return"]
 	},
 
 	"id4":{
 		num: "Image 4: ",
 		id: "Feminism",
 		image_src: "img/feminism-final.gif",
-		category: ["spin"]
+		movement: ["spin"],
+		looping: ["fade"],
+		speed: ["uniform"]
 	},
 
 	"id5":{
 		num: "Image 5: ",
 		id: "Food Puzzle",
 		image_src: "img/food-puzzle-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["return"],
+		speed: ["uniform"]
 	},
 
 	"id6":{
 		num: "Image 6: ",
 		id: "Fries Lighting",
 		image_src: "img/fries-lighting-final.gif",
-		category: ["sequential"]
+		movement: ["gradient"],
+		speed: ["uniform"]
 	},
 
 	"id7":{
 		num: "Image 7: ",
 		id: "Gillette Shaving",
 		image_src: "img/gillette-shaving-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["exit", "fade"]
 	},
 
 	"id8":{
 		num: "Image 8: ",
 		id: "Groom",
 		image_src: "img/groom-final.gif",
-		category: ["spin"]
+		movement: ["spin"],
+		looping: ["return"],
+		speed: ["uniform"]
 	},
 
 	"id9":{
 		num: "Image 9: ",
 		id: "Heinz Balloon",
 		image_src: "img/heinz-balloon-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["infinite"],
+		speed: ["uniform"]
 	},
 
 	"id10":{
 		num: "Image 10: ",
 		id: "Heinz Onion Rings",
 		image_src: "img/heinz-onion-rings-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["return"]
 	},
 
 	"id11":{
 		num: "Image 11: ",
 		id: "IBM Egg Astronaut",
 		image_src: "img/ibm-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["infinite"],
+		speed: ["uniform"]
 	},
 
 	"id12":{
 		num: "Image 12: ",
 		id: "It Was Never a Dress",
 		image_src: "img/it-was-never-a-dress-final.gif",
-		category: ["sequential"]
+		movement: ["sequential"],
+		looping: ["fade"],
+		speed: ["uniform"]
 	},
 
 	"id13":{
 		num: "Image 13: ",
 		id: "Ketchup Gaming",
 		image_src: "img/ketchup-gaming-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["exit", "fade"],
+		speed: ["uniform"]
 	}, 
 
 	"id14":{
 		num: "Image 14: ",
 		id: "KFC Opening",
 		image_src: "img/KFC-opening-soon-wiggle-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["return"]
 	}, 
 
 	"id15":{
 		num: "Image 15: ",
 		id: "Kiwi Battery",
 		image_src: "img/kiwi-battery-final.gif",
-		category: ["sequential"]
+		movement: ["sequential"]
 	}, 
 
 	"id16":{
 		num: "Image 16: ",
 		id: "Lego Plane",
 		image_src: "img/lego-plane-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["exit"],
+		speed: ["uniform"]
 	}, 
 
 	"id17":{
 		num: "Image 17: ",
 		id: "Lipton Tea Sunrise",
 		image_src: "img/lipton-sunset-final.gif",
-		category: ["linear", "gradient"]
+		movement: ["linear", "gradient"],
+		looping: ["return"]
 	}, 
 
 	"id18":{
 		num: "Image 18: ",
 		id: "McDonalds Easter",
 		image_src: "img/mcdonalds-easter-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["exit"]
 	}, 
 
 	"id19":{
 		num: "Image 19: ",
 		id: "McDonalds Filet-o-Fish Hook",
 		image_src: "img/mcdonalds-hook-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["exit"]
 	}, 
 
 	"id20":{
 		num: "Image 20: ",
 		id: "McDonalds Loading",
 		image_src: "img/McDonalds-loading-final.gif",
-		category: ["sequential"]
+		movement: ["sequential"]
 	}, 
 
 	"id21":{
 		num: "Image 21: ",
 		id: "McDonalds Wifi",
 		image_src: "img/mcdonalds-wifi-final.gif",
-		category: ["sequential"]
+		movement: ["sequential"],
+		speed: ["uniform"]
 	}, 
 
 	"id22":{
 		num: "Image 22: ",
 		id: "Mini Driving",
 		image_src: "img/mini-driving-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		speed: ["uniform"]
 	}, 
 
 	"id23":{
 		num: "Image 23: ",
 		id: "On Button",
 		image_src: "img/on-final.gif",
-		category: ["linear", "sequential"]
+		movement: ["linear", "gradient"],
+		looping: ["return"]
 	}, 
 
 	"id24":{
 		num: "Image 24: ",
 		id: "Oreo Eyes",
 		image_src: "img/oreo-eyes-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["return"]
 	}, 
 
 	"id25":{
 		num: "Image 25: ",
 		id: "Password",
 		image_src: "img/password-final.gif",
-		category: ["sequential"]
+		movement: ["sequential"]
 	}, 
 
 	"id26":{
 		num: "Image 26: ",
 		id: "Period Pad Maze",
 		image_src: "img/period-pad-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["infinite"]
 	}, 
 
 	"id27":{
 		num: "Image 27: ",
 		id: "Sushi Logo",
 		image_src: "img/sushi-logo-final.gif",
-		category: ["spin"]
+		movement: ["spin"],
+		looping: ["return"]
 	}, 
 
 	"id28":{
 		num: "Image 28: ",
 		id: "7-Eleven Traffic Light",
 		image_src: "img/traffic-light-final.gif",
-		category: ["sequential"]
+		movement: ["sequential"],
+		looping: ["return"],
+		speed: ["uniform"]
 	}, 
 
 	"id29":{
 		num: "Image 29: ",
 		id: "Trick-or-Treat",
 		image_src: "img/trick-or-treat-final.gif",
-		category: ["linear"]
+		movement: ["linear"],
+		looping: ["return"]
 	}, 
 
 	"id30":{
 		num: "Image 30: ",
 		id: "Wasting Life",
 		image_src: "img/wasting-life-final.gif",
-		category: ["expand"]
+		movement: ["expand"],
+		looping: ["return"],
+		speed: ["uniform"]
 	},
 
 	"id31":{
 		num: "Image 31: ",
 		id: "Pen Crown",
 		image_src: "img/pen-crown-final.gif", 
-		category: ["linear"]
-
+		movement: ["linear"],
+		looping: ["return"]
 	}
 
 }
--- js/helpers.js
+++ js/helpers.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 	loadHome();
 });
 
+// Movement functions.
+
 function loadDataLinear(){
 
 	$("#homepage").hide(); // Hides homepage.
@@ -26,9 +28,9 @@ function loadDataLinear(){
 	for (d in data){
 		var item = data[d];
 
-		for (c in item['category']){
+		for (c in item['movement']){
 
-			if (item['category'][c] == "linear"){
+			if (item['movement'][c] == "linear"){
 				count++;
 				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
 				var caption = item.num + item.id;
@@ -59,16 +61,506 @@ var setActiveLinear = function(type){
 	loadDataLinear();
 }
 
+function loadDataSpin(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['movement']){
+
+			if (item['movement'][c] == "spin"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveSpin = function(type){
+	console.log("Spin is set active");
+	changeHeader('Spin');
+	loadDataSpin();
+}
+
+function loadDataSequential(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['movement']){
+
+			if (item['movement'][c] == "sequential"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveSequential = function(type){
+	console.log("Sequential is set active");
+	changeHeader('Sequential');
+	loadDataSequential();
+}
+
+function loadDataGradient(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['movement']){
+
+			if (item['movement'][c] == "gradient"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveGradient = function(type){
+	console.log("Gradient is set active");
+	changeHeader('Gradient');
+	loadDataGradient();
+}
+
+function loadDataExpand(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['movement']){
+
+			if (item['movement'][c] == "expand"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveExpand = function(type){
+	console.log("Expand is set active");
+	changeHeader('Expand');
+	loadDataExpand();
+}
+
+function loadDataInfinite(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['looping']){
+
+			if (item['looping'][c] == "infinite"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveInfinite = function(type){
+	console.log("Infinite is set active");
+	changeHeader('Infinite');
+	loadDataInfinite();
+}
+
+function loadDataExit(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['looping']){
+
+			if (item['looping'][c] == "exit"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveExit = function(type){
+	console.log("Exit is set active");
+	changeHeader('Exit the Frame');
+	loadDataExit();
+}
+
+function loadDataFade(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['looping']){
+
+			if (item['looping'][c] == "fade"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveFade = function(type){
+	console.log("Fade is set active");
+	changeHeader('Fade');
+	loadDataFade();
+}
+
+function loadDataReturn(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['looping']){
+
+			if (item['looping'][c] == "return"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveReturn = function(type){
+	console.log("Return is set active");
+	changeHeader('Return to Original Position');
+	loadDataReturn();
+}
+
+// Uniform speed.
+function loadDataUniform(){
+
+	$("#homepage").hide(); // Hides homepage.
+	$("#parent").html(""); // Reset parent div.
+
+	var container_div = $("<div>");
+	$("#parent").append(container_div);
+
+	count = 0;
+
+	var row = $("<div>");
+	row.attr('class', "row");
+	$("row").html("");
+	$(container_div).append(row);
+
+	total = Object.keys(data).length;
+	currNum = 1;
+
+	for (d in data){
+		var item = data[d];
+
+		for (c in item['speed']){
+
+			if (item['speed'][c] == "uniform"){
+				count++;
+				var container_div = $("<div id='item" + item['id'] +"' class='container_div'>");
+				var caption = item.num + item.id;
+				container_div.append("<br>" + caption + "<br>");
+				var image = $("<img style='width:60%'>").attr("src", item['image_src']);
+				container_div.append(image);
+
+				if (currNum <= total){
+					currNum++;
+					col = $("<div>");
+					col.attr("class", "col-md-4");
+
+					$(row).append(col);
+					$(col).html(container_div);
+
+				}
+			}
+		}
+	}
+
+	changeCount();
+
+}
+
+var setActiveUniform = function(type){
+	console.log("Uniform speed is set active");
+	changeHeader('Uniform Speed');
+	loadDataUniform();
+}
+
+// Updates counter.
 var changeCount = function(){
 	$("#counter").empty();
 	$("#counter").append('Number of Animations: ' + count);
 }
 
+// Changes header.
 var changeHeader = function(name){
 	$("#header").empty();
 	$("#header").append(name);
 }
 
+// Loads homepage.
 var loadHome = function(){
 	$("#header").empty();
 	$("#counter").empty();
